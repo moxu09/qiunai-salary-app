7757852a69a4fa8c168773875230b5339411e640
moxu <moxu0927@gmail.com>
2026-07-26 04:58:05 +0800

    fix: defer translation until hydration completes

--- components/AppLanguageProvider.tsx
+++ components/AppLanguageProvider.tsx
@@ -222,17 +222,23 @@ export default function AppLanguageProvider() {
     document.documentElement.lang = locale;
     document.documentElement.dataset.locale = locale;
     window.localStorage.setItem(STORAGE_KEY, locale);
-    translateTree(document.body, locale);
-    const observer = new MutationObserver((mutations) => mutations.forEach((mutation) => {
-      if (mutation.type === "characterData") translateNode(mutation.target as Text, locale);
-      mutation.addedNodes.forEach((node) => {
-        if (node.nodeType === Node.TEXT_NODE) translateNode(node as Text, locale);
-        else if (node instanceof Element) translateTree(node, locale);
-      });
-      if (mutation.type === "attributes" && mutation.target instanceof Element) translateAttributes(mutation.target, locale);
-    }));
-    observer.observe(document.body, { childList: true, subtree: true, characterData: true, attributes: true, attributeFilter: [...attributes] });
-    return () => observer.disconnect();
+    let observer: MutationObserver | null = null;
+    const startTimer = window.setTimeout(() => {
+      translateTree(document.body, locale);
+      observer = new MutationObserver((mutations) => mutations.forEach((mutation) => {
+        if (mutation.type === "characterData") translateNode(mutation.target as Text, locale);
+        mutation.addedNodes.forEach((node) => {
+          if (node.nodeType === Node.TEXT_NODE) translateNode(node as Text, locale);
+          else if (node instanceof Element) translateTree(node, locale);
+        });
+        if (mutation.type === "attributes" && mutation.target instanceof Element) translateAttributes(mutation.target, locale);
+      }));
+      observer.observe(document.body, { childList: true, subtree: true, characterData: true, attributes: true, attributeFilter: [...attributes] });
+    }, 350);
+    return () => {
+      window.clearTimeout(startTimer);
+      observer?.disconnect();
+    };
   }, [locale]);
 
   useEffect(() => {
